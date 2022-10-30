f50399bb86a544d5f93662d7b7d47d5bd11ebce2
samyakkhatua <samyakkhatua@duck.com>
2022-10-30 23:31:22 +0530

    port updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV !== "production"){
   require("dotenv").config();
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
